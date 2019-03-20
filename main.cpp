dfa5d41f39c7d38315888f469f7b9d3219845f3a
liuyujun <liuyujun@fingera.cn>
2019-03-20 21:50:48 +0800

    add printf

--- main.cpp
+++ main.cpp
@@ -6,16 +6,17 @@
 using coroutine::Context;
 
 Context *myRoutine(Context *ctx) {
-  //printf("协程 第一次: %p\n", ctx->getArgs());
+  printf("协程 第一次: %p\n", ctx->getArgs());
   ctx = ctx->Call((void *)0x2222);
-  //printf("协程 第二次: %p\n", ctx->getArgs());
+  printf("协程 第二次: %p\n", ctx->getArgs());
   ctx = ctx->Call((void *)0x4444);
-  //printf("协程 第三次: %p\n", ctx->getArgs());
+  printf("协程 第三次: %p\n", ctx->getArgs());
   return ctx->setArgs((void *)0x6666);
 }
 
 int main(int argc, char const *argv[]) {
   char stack[1024 * 100];
+  printf("协程堆栈: %p => %p\n", stack, stack + sizeof(stack));
   Context routine(myRoutine, stack + sizeof(stack));
   auto r = routine.Call((void *)0x1111);
   printf("主函数 第一次返回: %p %d\n", r->getArgs(), r == &routine);
