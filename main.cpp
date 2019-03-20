10a85322d770eba38c0afedcf5d784e503949b42
liuyujun <liuyujun@fingera.cn>
2019-03-20 21:53:40 +0800

    add test

--- main.cpp
+++ main.cpp
@@ -5,9 +5,16 @@
 
 using coroutine::Context;
 
+Context *subRoutine(Context *ctx) {
+  printf("小协程 第一次: %p\n", ctx->getArgs());
+  return ctx->setArgs((void *)0x2222);
+}
+
 Context *myRoutine(Context *ctx) {
+  char stack[1024 * 5];
+  Context sub(subRoutine, stack + sizeof(stack));
   printf("协程 第一次: %p\n", ctx->getArgs());
-  ctx = ctx->Call((void *)0x2222);
+  ctx = ctx->Call(sub.Call(nullptr)->getArgs());
   printf("协程 第二次: %p\n", ctx->getArgs());
   ctx = ctx->Call((void *)0x4444);
   printf("协程 第三次: %p\n", ctx->getArgs());
